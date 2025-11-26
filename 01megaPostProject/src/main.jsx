67fbbb6bb8ffb0784ee189cbe9ce9fd080fbd27a
abhishek goud <goud87041@gmail.com>
2025-11-26 21:42:33 +0530

    add some mini mistake in store

--- 01megaPostProject/src/main.jsx
+++ 01megaPostProject/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './store/store.js'
+// import store from './store/store.js'
+import Store from './Store/Store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './Components/Index.js'
@@ -79,7 +80,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
     <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
