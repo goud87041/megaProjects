370eff0af55846fec62e0fb180f3ea881bde70b2
abhishek goud <goud87041@gmail.com>
2025-12-07 10:10:01 +0530

    add some utils

---
+++ 02megaBackendProjectWithExpress/src/utils/ApiRespone.js
@@ -0,0 +1,13 @@
+import { Error } from "mongoose";
+
+class ApiResponse{
+    constructor(
+        statusCode,data,massage = "Success"
+    ){
+        this.statusCode = statusCode,
+        this.data = data,
+        this.massage  = massage < 400
+    }
+}
+
+export {ApiResponse}
---
+++ 02megaBackendProjectWithExpress/src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+import { response } from "express"
+
+const asyncHandler = (requestHandler)=>{
+    Promise.response(requestHandler(req,res,next)).catch((err)=>{
+        next(err)
+    })
+}
+
+export {asyncHandler}
+
+
+
+
+// const asyncHandler =  (fn)=> async (req,res,next)=>{
+//     try {
+        
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({success : false,massage : err.massage})
+//     }
+// }
